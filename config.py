487d7db96b4806a430817f1d340722206d53b965
Illia Boiko (selplacei) <ilyaviaik@gmail.com>
2021-02-27 17:28:29 -0800

    Add default paths to the config

--- config.py
+++ config.py
@@ -3,12 +3,40 @@ from pathlib import Path
 
 
 if (_config_home := os.path.expandvars('$XDG_CONFIG_HOME')) != '$XDG_CONFIG_HOME':
-	KONFSAVE_DATA_PATH = Path(_config_home) / 'konfsave'
+	CONFIG_HOME = Path(_config_home)
 else:
-	KONFSAVE_DATA_PATH = Path.home() / '.config' / 'konfsave'
+	CONFIG_HOME = Path.home() / '.config'
 
+
+KONFSAVE_DATA_PATH = CONFIG_HOME / 'konfsave'
 KONFSAVE_CURRENT_PROFILE_PATH = KONFSAVE_DATA_PATH / 'current_profile'
 
-PATHS_TO_SAVE = {  # These paths are relative to the home directory.
+_XDG_CONFIG_PATHS_TO_SAVE = {
+	# These paths are relative to $XDG_CONFIG_HOME (~/.config).
+	'gtk-2.0',
+	'gtk-3.0',
+	'Kvantum',
 	
+	'konfsave/config.py',
+	'dolphinrc',
+	'konsolerc',
+	'kcminputrc',
+	'kdeglobals',
+	'kglobalshortcutsrc',
+	'klipperrc',
+	'krunnerrc',
+	'kscreenlockerrc',
+	'ksmserverrc',
+	'kwinrc',
+	'kwinrulesrc',
+	'plasma-org.kde.plasma.desktop-appletsrc',
+	'plasmarc',
+	'plasmashellrc',
+	'gtkrc',
+	'gtkrc-2.0'
 }
+
+PATHS_TO_SAVE = set(map(lambda p: os.path.join(Path.home(), p), {
+	# These paths are relative to the home directory.
+	'.kde4'
+})) | set(map(lambda p: os.path.join(CONFIG_HOME, p), _XDG_CONFIG_PATHS_TO_SAVE))
